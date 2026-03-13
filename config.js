bda83ec263c3ab3603e66c14a758f0b558d21069
Jb papa 71 <jbrk9088@gmail.com>
2026-03-13 15:04:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 module.exports = {
 
 BOT_NAME: "JB PAPA 71 ☠️",
-
 OWNER: "JB PAPA 71",
-
 PREFIX: ".",
 
 MENU_IMAGE: "https://files.catbox.moe/uap3gf.png",
 
 WHATSAPP_CHANNEL: "https://whatsapp.com/channel/0029Vb69yTi5PO0rX16dFQ1L",
-
 TELEGRAM_GROUP: "https://t.me/jb_papa71"
 
 }
