74d95288ec8238a6f3e24e5f30c25d24aad18f6a
Jb papa 71 <jbrk9088@gmail.com>
2026-03-13 14:54:20 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+BOT_NAME: "JB PAPA 71 ☠️",
+
+OWNER: "JB PAPA 71",
+
+PREFIX: ".",
+
+MENU_IMAGE: "https://files.catbox.moe/uap3gf.png",
+
+WHATSAPP_CHANNEL: "https://whatsapp.com/channel/0029Vb69yTi5PO0rX16dFQ1L",
+
+TELEGRAM_GROUP: "https://t.me/jb_papa71"
+
+}
